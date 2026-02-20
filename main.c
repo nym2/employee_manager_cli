9fe117300ff8303b63b765c8350dc2f3006237f0
nym2 <praisepamba@gmail.com>
2026-02-21 00:34:02 +0300

    feat: implement menu system for user interaction

--- main.c
+++ main.c
@@ -11,8 +11,18 @@ typedef struct {
     Date hired;
 } Employee;
 
+int menu();
+
 int main(void) {
     Employee employees[100];
     int count = 0;
     return 0;
+}
+
+int menu() {
+    int choice;
+    printf("WELCOME TO THE EMPLOYEE MANAGER, SELECT AN OPTION\n");
+    printf("0. Add Employee\n1. Remove Employee\n2. Give Raise\n3. Find Oldest Employee\n4. Print All Employees\n5. Exit\n");
+    scanf("%d", &choice);
+    return choice;
 }
